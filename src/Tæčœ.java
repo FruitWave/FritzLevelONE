9671ff7199fc387e17c1353e31eed9796adec41a
FruitWave <Guest@wts10.attlocal.net>
2015-08-21 19:29:43 -0700

    Third Taecoe commit
    The third Taecoe class commit.

--- src/Tæčœ.java
+++ src/Tæčœ.java
@@ -1,12 +1,28 @@
-
 public class Tæčœ {
-String sauce;
-String meat;
-	public Tæčœ(String sauce, String meat){
+	String sauce;
+	String meat;
+
+	Tæčœ cornStuff = new Tæčœ("pork", "guacamole");
+
+	public Tæčœ(String sauce, String meat) {
 		this.meat = meat;
 		this.sauce = sauce;
-		
+	}
 
+	public String setSauce(String sauce) {
+		this.sauce = sauce;
+	}
+
+	public String setMeat(String meat) {
+		this.meat = meat;
+	}
+
+	public String getSauce(String sauce) {
+		return sauce;
+	}
+
+	public String getMeat(String meat) {
+		return meat;
 	}
 
 }
