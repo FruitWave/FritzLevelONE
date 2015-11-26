899772a3502b7f980552280a865493bbc9748361
Fritzitunes <Fritzitunes@Wendys-iMac.local>
2015-11-25 18:33:23 -0800

    The Committee
    The Summary is so cool it’s now spelled with an ’S’!

---
+++ src/Destini.java
@@ -0,0 +1,10 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JApplet;
+public class Destini {
+JFrame proximus = new JFrame();
+
+}
