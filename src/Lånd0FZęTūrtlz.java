8511080f8e631e699f06081af606b5c9843936f0
Lucas Baizer <titaniumsapphire32@gmail.com>
2016-01-14 17:24:21 -0800

    Commit January 14, 2015
    The summary shall enlighten you. I am not a teacher.

---
+++ src/Lånd0FZęTūrtlz.java
@@ -0,0 +1,37 @@
+//Lånd0FZęTūrtlz
+
+
+import java.util.Random;
+
+import org.teachingextensions.logo.MultiTurtlePanel;
+import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.Turtle;
+
+public class Lånd0FZęTūrtlz {
+	public static void main(String[] args) {
+		MultiTurtlePanel paneltopia = new MultiTurtlePanel();
+		String galafrey = "https://farm2.staticflickr.com/1104/752631367_5c5d474ba5_o.jpg";
+	
+		/* 1. Get the panel to show */
+		paneltopia.showPanel();
+		/* 2. Set the background image of the panel to the Galapagos Islands */
+paneltopia.setBackgroundImage(galafrey);
+		/* 3. Instantiate a Turtle and add it to the panel */
+		Turtle soupmantheultimateturtleofalltimeohyeahthatiswhatitypeduhhuhwoohoo = new Turtle();
+		paneltopia.addTurtle(soupmantheultimateturtleofalltimeohyeahthatiswhatitypeduhhuhwoohoo);
+		/* 4. Put [no] Turtles on the beach. */
+		int i =1;
+		int weird = new Random().nextInt(i*15);
+		System.out.println("weird: " + weird);
+for (; i < weird; i++) {
+	Turtle a = new Turtle();
+	paneltopia.addTurtle(a);
+	a.setX(new Random().nextInt(i*15));
+	a.setY(new Random().nextInt(i*15));
+	System.out.println(i);
+	
+	}
+
+	}
+}
+
