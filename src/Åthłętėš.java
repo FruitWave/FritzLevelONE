e6396d6917137e7b0f6fcfc84b3d4e533c97b7b6
Livie0423 <league@WTS9.attlocal.net>
2016-08-10 16:57:33 -0700

    Magic
    It’s stuff.

---
+++ src/Åthłętėš.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+
+public class Åthłętėš {
+	static String geolocationalparaathes = "Earth";
+	String excletevraan;
+	int incenseplait; // bn
+	int iphonenumm;
+	static int deadp007 = 0; // count
+
+	Åthłętėš(String exclenothis, int incenothis, int iphonothis) {
+		this.excletevraan = exclenothis;
+		this.incenseplait = incenothis;
+		this.iphonenumm = iphonothis;
+		deadp007 = deadp007 + 1;
+
+	}
+
+	public static void main(String[] args) {
+		int x = new Random().nextInt(1001);
+		int pokemlulz = new Random().nextInt(1001);
+
+		Åthłętėš eliminum = new Åthłętėš("--_-'.'-_--", x, pokemlulz);
+
+		System.out.println(eliminum.incenseplait);
+		System.out.println(eliminum.deadp007);
+		Åthłętėš aluminium = new Åthłętėš("--_-+.+-_--", x, pokemlulz);
+		System.out.println(aluminium.incenseplait);
+		System.out.println(aluminium.deadp007);
+		System.out.println(x);
+		System.out.println(pokemlulz);
+	}
+
+}
